da0dcbd6eef11fb80b0b353405a01f8e8c629eae
N1ceboat <yuyun0124@gmail.com>
2019-05-10 02:20:42 +0900

    1. 주인공 우주선 만들기, 맵 제작

---
+++ Assets/Sources/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    float speed = 5;    // 속도
+    float tilt = 5;     // 회전값
+
+    void Start() {
+
+    }
+    
+    void FixedUpdate() {
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(dirX, 0, dirY);
+
+        GetComponent<Rigidbody>().velocity = movement * speed;
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0, 0, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+}
