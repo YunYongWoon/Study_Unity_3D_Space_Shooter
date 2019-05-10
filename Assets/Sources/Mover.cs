7167425062bffe5b64a54897ae32a169e71f16e8
N1ceboat <yuyun0124@gmail.com>
2019-05-10 11:34:05 +0900

    3. 적 구현하기

--- Assets/Sources/Mover.cs
+++ Assets/Sources/Mover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
-    float speed = 5;
+    public float speed = 5;
     void Start() {
         GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -1) * speed;
     }
